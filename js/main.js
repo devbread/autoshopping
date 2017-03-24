8c1041e7caf4d3f1e476d32a6ff204d6186ae2c9
devbread <flores@cabeza.cl>
2017-03-24 16:17:58 -0300

    últimos cambios

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+    $(document).ready(function(){
 
 
 // $('body').flowtype({
@@ -121,25 +121,25 @@ $(document).ready(function(){
 
 
 
-(function($, window) {
-    var $ls;
-    function autoheight() {
-        var max = 0;
-        $ls.each(function() {
-            $t = $(this);
-            $t.css('height','');
-            max = Math.max(max, $t.height());
-        });
-        $ls.height(max);
-    }
-    $(function() {
-        $ls = $('.my-inline-block-class'); // the inline-block selector
-        autoheight(); // first time
-        $ls.on('load', autoheight); // when images in content finish loading
-        $(window).load(autoheight); // when all content finishes loading
-        $(window).resize(autoheight); // when the window size changes
-    });
-})(jQuery, window);
+// (function($, window) {
+//     var $ls;
+//     function autoheight() {
+//         var max = 0;
+//         $ls.each(function() {
+//             $t = $(this);
+//             $t.css('height','');
+//             max = Math.max(max, $t.height());
+//         });
+//         $ls.height(max);
+//     }
+//     $(function() {
+//         $ls = $('.my-inline-block-class'); // the inline-block selector
+//         autoheight(); // first time
+//         $ls.on('load', autoheight); // when images in content finish loading
+//         $(window).load(autoheight); // when all content finishes loading
+//         $(window).resize(autoheight); // when the window size changes
+//     });
+// })(jQuery, window);
 
 
 // $(window).on("load, resize", function() {
